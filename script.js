27a47c5d1ca4bc0bd7149368a7c2a7341a8f62fd
Estanler Aleman <Esencymusic@gmail.com>
2025-07-30 13:47:06 -0400

    Create script.js
    Added a auto-trigger function so that when clicked, my contact can be automatically imported into mobile contact app. For desktop devices it will most likely be downloaded as a .vcf file.

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('download-vcard-btn').addEventListener('click', () => {
+  // this will “click” the hidden link, auto‑starting the .vcf download
+  document.getElementById('download-vcf').click();
+});
